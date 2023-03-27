a6e8562925b8e3ee5b59fc063f47eed8150a3647
jaganjavid <jagantutor1@gmail.com>
2023-03-27 22:23:23 +0530

    react started

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  const title = "This is my react app";
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default App;
