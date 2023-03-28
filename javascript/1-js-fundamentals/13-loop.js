08652f92968c9f363b05593d685998d7a749024e
jaganjavid <jagantutor1@gmail.com>
2023-03-28 22:50:07 +0530

    array concept

--- javascript/1-js-fundamentals/13-loop.js
+++ javascript/1-js-fundamentals/13-loop.js
@@ -2,18 +2,18 @@
 
 // Loop can execute a block of code number of times
 
-for(let i = 0; i <= 10; i++){
-  if(i === 2){
-    console.log(`2 is printed`);
-    continue;
-  }
+// for(let i = 0; i <= 10; i++){
+//   if(i === 2){
+//     console.log(`2 is printed`);
+//     continue;
+//   }
 
-  if(i === 5){
-    console.log("Hit 5 stop the loop");
-    break;
-  }
-  console.log(i);
-}  
+//   if(i === 5){
+//     console.log("Hit 5 stop the loop");
+//     break;
+//   }
+//   console.log(i);
+// }  
 
 // let i = 0;
 // while (i < 10) {
@@ -27,20 +27,46 @@ for(let i = 0; i <= 10; i++){
 //   i++;
 // }
 
-var furits = ["apple", "orange", "perl"];
+// var furits = ["apple", "orange", "perl"];
 
-const f = furits.forEach((furit) => {
-console.log(furit);
+// const f = furits.forEach((furit) => {
+// console.log(furit);
+// })
+
+
+// const fu = furits.map((furit) => {
+// return furit;
+// })
+
+// const fi = furits.filter((furit) => {
+// return furit === "apple";
+// })
+
+// console.log(fi);
+// console.log(furits);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const vegs = ["carrot"];
+
+// const result = fruits.at(2);
+
+// const result = fruits.concat(vegs);
+
+
+const ages = [32,33, 34, 17];
+
+const result = ages.every((age) => {
+  return age >= 18;
 })
 
 
-const fu = furits.map((furit) => {
-return furit;
-})
+// const result = ages.find((age) => {
+//   return age >= 18;
+// })
 
-const fi = furits.filter((furit) => {
-return furit === "apple";
-})
+// const result = ages.findIndex((age) => {
+//   return age >= 18;
+// })
 
-console.log(fi);
-console.log(furits);
+console.log(result);
+
